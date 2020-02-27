a616bb67f192642f629175459873c9107bc32fec
Amin Arria <arria.amin@gmail.com>
2020-02-27 12:15:00 -0300

    Added koans for python interop

---
+++ solutions/PythonInterop.jl
@@ -0,0 +1,36 @@
+module PythonInterop
+
+using Pkg
+Pkg.add("PyCall")
+using PyCall
+
+function __init__()
+    py"""
+    import numpy as np
+
+    def sinpi(x):
+        return np.sin(np.pi * x)
+
+    def pyfib(n, fib):
+        # print("hello from python")
+        if(n < 2):
+            return n
+        else:
+            return fib(n-1, pyfib) + fib(n-2, pyfib)
+    """
+end
+
+function py_sinpi(x)
+    py"sinpi"(x)
+end
+
+function get_pyfib()
+    py"pyfib"
+end
+
+function jlfib(n, fib)
+    # println("hello from julia")
+    n < 2 ? n : fib(n-1, fib) + fib(n-2, fib)
+end
+
+end
