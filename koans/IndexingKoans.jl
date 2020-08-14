a44c801d4dc967e8ba441a7052339727aa0041e5
Camilo1704 <camilo1704@gmail.com>
2020-08-14 17:56:18 -0300

    indexing koans added

--- koans/IndexingKoans.jl
+++ koans/IndexingKoans.jl
@@ -1,2 +1,30 @@
 module IndexingKoans
+
+#=
+    links:
+        - https://docs.julialang.org/en/v1/manual/arrays/#man-array-indexing-1
+=#
+
+function first_element(vector)
+end
+
+function last_element(vector)
+end
+
+function range_indices(array)
+    # return all values of 2D array except for the first and the last columns
+end
+
+function indexed_assignment(array)
+    # multiply x2 and replace the first and the last columns of the 2D array
+end
+
+function elementwise_indexing_assigment(array, value)
+    #assign the value to the first and second columns of the 2D array
+end
+
+function apply_mask(array, condition)
+    # return the values where the condition is true
+end
+
 end
---
+++ solutions/IndexingKoans.jl
@@ -0,0 +1,38 @@
+module IndexingKoans
+
+#=
+    links:
+        - https://docs.julialang.org/en/v1/manual/arrays/#man-array-indexing-1
+=#
+
+function first_element(vector)
+    return vector[1]
+end
+
+function last_element(vector)
+    return vector[end]
+end
+
+function range_indices(array)
+    # return all values of 2D array except the first and the last columns
+    return array[:, 2:size(array)[2]-1]
+end
+
+function indexed_assignment(array)
+    # multiply x2 the first and the last columns of the 2D array
+    array[:,[1,end]] = 2*array[:,[1,end]]
+    return array
+end
+
+function elementwise_indexing_assigment(array, value)
+    # assign the value to the first and second columns of the 2D array
+    array[:,[1,2]] .= value
+    return  array
+end
+
+function apply_mask(array, condition)
+    # return the values where the condition is true
+    return array[map( x-> condition(x), array)]
+end
+
+end
