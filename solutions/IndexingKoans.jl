5131ad7fc06945dac4721b13a0f1fbc48f6d21f6
Camilo1704 <camilo1704@gmail.com>
2020-08-14 17:58:44 -0300

    typo

--- solutions/IndexingKoans.jl
+++ solutions/IndexingKoans.jl
@@ -14,18 +14,18 @@ function last_element(vector)
 end
 
 function range_indices(array)
-    # return all values of 2D array except the first and the last columns
+    # return all values of the 2D array except the first and the last columns
     return array[:, 2:size(array)[2]-1]
 end
 
 function indexed_assignment(array)
-    # multiply x2 the first and the last columns of the 2D array
+    # multiply x2 and replace the values of the first and the last columns of the 2D array
     array[:,[1,end]] = 2*array[:,[1,end]]
     return array
 end
 
 function elementwise_indexing_assigment(array, value)
-    # assign the value to the first and second columns of the 2D array
+    # assign the value to each position in the first and second columns of the 2D array
     array[:,[1,2]] .= value
     return  array
 end
