f11b720bfedb76799bbeec2e57bde57c789b450a
Camilo1704 <camilo1704@gmail.com>
2020-08-18 17:00:08 -0300

    koans added

--- koans/IterationKoans.jl
+++ koans/IterationKoans.jl
@@ -1,2 +1,28 @@
 module IterationKoans
+
+function range_iteration(n, m, step)
+    #return the sum of the elements iterating between n and m with step "step"
+    sum = 0
+    for i in n:step:m
+        sum+=i
+    end
+    return sum
+end
+
+function efficient_vector_iteration(array)
+    #return the multiplication of the element by its index
+    for i in eachindex(array)
+        array[i]*= i
+    end
+    return array
+
+function chain_iterations(array, tuple)
+    #chain the iteration over the array and the tuple and return a new vector with its elements
+    new_array = []
+    for i in chain(array, tuple)
+        append!(new_array,i)
+    end
+    return new_array
+
+function
 end
