921abfdbe9d8810b0e017947817225a7ae0fd0e3
Camilo1704 <camilo1704@gmail.com>
2020-08-19 11:39:26 -0300

    iteration koans added

--- tests/tests.jl
+++ tests/tests.jl
@@ -70,6 +70,15 @@ end
 
 using IterationKoans
 @testset "Iteration" begin
+    @test IterationKoans.range_iteration(1, 5, 1) == 15
+    @test IterationKoans.range_iteration(0, 10.1, 0.1) == 515.1
+    @test IterationKoans.efficient_vector_iteration([1, 3, 5, 6]) == [1, 6, 15, 24]
+    @test IterationKoans.efficient_vector_iteration([0, 4, 2, 7]) == [0, 8, 6, 28]
+    @test IterationKoans.chain_iterations([3,4,5], ['r', 'f']) == [3, 4, 5, 'r', 'f']
+end
+
+using IterationKoans
+@testset "Iteration" begin
 end
 
 using ModuleKoans
