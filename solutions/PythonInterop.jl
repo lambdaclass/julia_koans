01a5f8cb08e5130813b0da3cd74bfb06d44804a7
Camilo1704 <camilo1704@gmail.com>
2020-08-24 17:06:28 -0300

    project toml added

--- solutions/PythonInterop.jl
+++ solutions/PythonInterop.jl
@@ -1,9 +1,9 @@
 module PythonInterop
 
-using Pkg
-# Pkg.add("PyCall")
-Pkg.add(PackageSpec(name="PyCall", rev="master"))
-using PyCall
+# using Pkg
+# # Pkg.add("PyCall")
+# Pkg.add(PackageSpec(name="PyCall", rev="master"))
+# using PyCall
 
 function __init__()
     py"""
