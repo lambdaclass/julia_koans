a831d0bdd9a696a431917732b29bd286f6418522
Camilo1704 <camilo1704@gmail.com>
2020-08-28 12:27:39 -0300

    some text added with documentation links and motivation

--- koans/ArrayKoans.jl
+++ koans/ArrayKoans.jl
@@ -1,4 +1,4 @@
- 
+
 # An array is the basic data structure to work with a collection of objects in Julia. This objects
 # can be of the same type or they can be of different types.
 # Arrays can even store another arrays inside of them, which becomes useful to work with matrices, among
@@ -12,63 +12,117 @@ module ArrayKoans
 
 
 function zeros_1d(n)
-    # A function that given an integer n, returns a 1D array with n zeros.
-    # Example: zeros_1d(3) = [0.0, 0.0, 0.0]
-
+    #=
+     Write a function that given an integer n, returns a 1D array with n zeros.
+     Ex: zeros_1d(3) = [0.0, 0.0, 0.0]
+    =#
 end
 
 function zeros_2d(n)
-    # A function that given an integer n, returns a nxn matrix of zeros.
-    # Example: zeros_2d(2) = [0.0 0.0; 0.0 0.0]
+    #=
+     Write a function that given an integer n, returns a nxn matrix of zeros.
+     Ex: zeros_2d(2) = [0.0 0.0; 0.0 0.0]
+    =#
 end
 
 function array_shape(array)
-    # return the shape of the array
+    #=
+     Return the shape of the array. When we work with arrays, the shape of the
+     array is a important properpy since many operations like the dot product and
+     matrix multiplications require specific array shapes.
+     Ex: array = 3×2 Array{Int64,2}: 4 5 ; return (3, 2)
+                                     5 6
+                                     6 7
+    =#
 end
 
 function array_dimension(array)
-    #return the dimension of the array
+    #=
+     Return the dimension of the array. With the shape, the dimension of the array are
+     two of the most important properties, frequently used when we work with arrays.
+     Ex: array = 3×2 Array{Int64,2}: 4 5 ; return 2
+                                     5 6
+                                     6 7
+    =#
 end
 
 function num_elements(array)
-    #return the number of elements of the array
+    #=
+     Return the number of elements of the array.
+     Ex: array = 3×2 Array{Int64,2}: 4 5 ; return 6
+                                     5 6
+                                     6 7
+    =#
 end
 
 function empty_array(mytype)
-    #return an empty 1D array of type mytype
+    #=
+     Return an empty 1D array of type mytype
+    =#
 end
 
 function range_array(start, stop, step)
-    #use the collect function to return an array
+    #=
+     Arrays having ordered values within a range are frequently used, for iteration
+     for example. Use the collect function to return an array.
+     Ex: start = 1; stop = 5; step = 2; return [1, 3, 5]
+    =#
 end
 
 function filled_array(val, size)
-    #use create an array of size "size" filling all values with val
+    #=
+     Create an array of size "size" filling all values with val.
+     Ex: val = 2; size = (2,4); return:  [2 2 2 2; 2 2 2 2]
+    =#
 end
 
 function reshape_array(array)
-    #reshape the array to be 1D
+     #=
+     Since the shape of the array is an important property, reshaping the
+     array is an useful tool, being a convenient operation most of the time.
+     Write a function to reshape the array "array" to be 1D.
+     Example: array = [ 3 4 5; 6 7 8], return  [ 3, 4, 5, 6, 7, 8]
+    =#
 end
 
 function copy_shape(array)
-   # return a new array with the shape of array
+    #=
+     Return a new array with the shape of array
+    =#
 end
 
 function entire_column(array, col)
-    #return the entire column "col" of a 2D array
+    #=
+     Often we have an array-type data structure where each column represents a property in the data.
+     Accessing and returning a particular column is therefore commonly done.
+     Return the entire column "col" of a 2D array.
+     Ex: array [3 4 5; 6 7 8; 9 10 11]; col = 3; return [5; 8; 11]
+    =#
 end
 
 function count_repetitions(array, a)
-    #return how many times "a" is in the array
+    #=
+     Counting repetitions of a specific value is a common operation when
+     we work with arrays. Write a function counting how many times "a" is in the array.
+     Ex: array = [ 4 5 6 ;  6 6 8]; a = 6; return 3.
+    =#
 end
 
 function array_of_functions()
-    #return an 1D-array having as elements the functions sin, cos, tan
+    #=
+     Return an 1D-array having as elements the functions sin, cos, tan.
+     This is useful, for example, if we want to evaluate them over
+     a data structure vectorizing them.
+    =#
 end
 
 function find_index(array, a)
-    #= return an array of indices where there is an "a" in the array
-    the returned value must be an array with CartesianIndex elements
+    #=
+     Return an array of indices where there is an "a" in the array,
+     the returned value must be an array with CartesianIndex elements.
+     Ex: array = [3 4 5; 5 6 6]; a = 5;
+     return Array{CartesianIndex{2},1}:  CartesianIndex(2, 1)
+                                         CartesianIndex(1, 3)
     =#
 end
 
--- solutions/ArrayKoans.jl
+++ solutions/ArrayKoans.jl
@@ -1,73 +1,152 @@
 module ArrayKoans
 
 function array_1d(n)
-    # return a 1D array of zeros with length n
+    #=
+     Write a function that given an integer n, returns a 1D array with n zeros.
+     Here is the link to the documentation:
+     - https://docs.julialang.org/en/v1/base/arrays/#Base.zeros
+     Ex: zeros_1d(3) = [0.0, 0.0, 0.0]
+    =#
     return zeros(n)
 end
 
 function array_2d()
-    # return a 2x2 matrix of zeros
+    #=
+     Return a 2x2 matrix of zeros.
+     Here is the link to the documentation:
+     - https://docs.julialang.org/en/v1/base/arrays/#Base.zeros
+     Ex: return [0 0; 0 0]
+    =#
     return zeros(2,2)
 end
 
 function array_shape(array)
-    # return the shape of the array
+     #=
+     Return the shape of the array. When we work with arrays, the shape of the
+     array is a important property since many operations like the dot product and
+     matrix multiplications require specific array shapes.  return the shape of the array
+     Here is the documentation
+     to the operation in Julia for the shape of the array:
+        - https://docs.julialang.org/en/v1/base/arrays/#Base.size
+    =#
     return size(array)
 end
 
 function array_dimension(array)
-    #return the dimension of the array
+    #=
+     Return the dimension of the array. With the shape, the dimension of the array are
+     two of the most important properties, frequently used when we work with arrays.
+     Here is the link to the documentation:
+     - https://docs.julialang.org/en/v1/base/arrays/#Base.ndims
+     Ex: array = 3×2 Array{Int64,2}: 4 5 ; return 2
+                                     5 6
+                                     6 7
+    =#
     return ndims(array)
 end
 
 function num_elements(array)
-    #return the number of elements of the array
+    #=
+     Return the number of elements of the array.
+     Here is the link to the documentation:
+     - https://docs.julialang.org/en/v1/base/arrays/#Base.length-Tuple{AbstractArray}
+     Ex: array = 3×2 Array{Int64,2}: 4 5 ; return 6
+                                     5 6
+                                     6 7
+    =#
     return length(array)
 end
 
 function empty_array(mytype)
-    #return an empty 1D array of type mytype
+    #=
+     Return an empty 1D array of type mytype.
+     Here is a useful link:
+     - https://riptutorial.com/julia-lang/example/20451/initialize-an-empty-array
+    =#
     return mytype[]
 end
 
 function range_array(start, stop, step)
-    #use the collect function to return an array
+    #=
+     Arrays having ordered values within a range are frequently used, for iteration
+     for example. Use the collect function to return an array.
+     Here is the link to the documentation to the function used:
+        - https://docs.julialang.org/en/v1/base/collections/#Base.collect-Tuple{Any}
+     Ex: start = 1; stop = 5; step = 2; return [1, 3, 5]
+    =#
     return collect(start:step:stop)
 end
 
 function filled_array(val, size)
-    #use create an array of size "size" filling all values with val
+    #=
+     Create an array of size "size" filling all values with val.
+     Here is some useful documentation to the function used:
+      - https://docs.julialang.org/en/v1/base/arrays/#Base.fill
+     Ex: val = 2; size = (2,4); return:  [2 2 2 2; 2 2 2 2]
+    =#
     return fill(val, size)
 end
 
 function reshape_array(array)
-    #reshape the array to be 1D
+    #=
+     Since the shape of the array is an important property, reshaping the
+     array is an useful tool, being a convenient operation most of the time.
+     Write a function to reshape the array "array" to be 1D.
+     Here is the link to the documentation:
+     - https://docs.julialang.org/en/v1/base/arrays/#Base.reshape
+     Example: array = [ 3 4 5; 6 7 8], return  [ 3, 4, 5, 6, 7, 8]
+    =#
     return reshape(array, prod(size(array)))
 end
 
 function copy_shape(array)
-   # return a new array with the shape of array
+   #=
+    Return a new array with the shape of array.
+    Here is the link to the function used:
+     - https://docs.julialang.org/en/v1/base/arrays/#Base.similar
+    =#
    return similar(array)
 end
 
 function entire_column(array, col)
-    #return the entire column "col" of a 2D array
+    #=
+     Often we have an array-type data structure where each column represents a property in the data.
+     Accessing and returning a particular column is therefore commonly done.
+     Return the entire column "col" of a 2D array.
+     Ex: array [3 4 5; 6 7 8; 9 10 11]; col = 3; return [5; 8; 11]
+    =#
     return (array[:,col])
 end
 
 function count_repetitions(array, a)
-    #return how many times "a" is in the array
+    #=
+     Counting repetitions of a specific value is a common operation when
+     we work with arrays. Write a function counting how many times "a" is in the array.
+     Here is the link to the documentation of count:
+     - https://docs.julialang.org/en/v1/base/collections/#Base.count
+     Ex: array = [ 4 5 6 ;  6 6 8]; a = 6; return 3.
+    =#
     return count(i-> i==a, array)
 end
 
 function array_of_functions()
-    #return an 1D-array having as elements the functions sin, cos, tan
+    #=
+     Return an 1D-array having as elements the functions sin, cos, tan.
+     This is useful, for example, if we want to evaluate them over
+     a data structure vectorizing them.
+    =#
     return [sin cos tan]
 end
 
 function find_index(array, a)
-    #= return an array of indices where there is an "a" in the array,
-    the returned value must be an array with CartesianIndex elements
+    #=
+     Return an array of indices where there is an "a" in the array,
+     the returned value must be an array with CartesianIndex elements.
+     Here is the link to the documentation:
+      - https://docs.julialang.org/en/v1/base/arrays/#Base.findall-Tuple{Any}
+     Ex: array = [3 4 5; 5 6 6]; a = 5;
+     return Array{CartesianIndex{2},1}:  CartesianIndex(2, 1)
+                                         CartesianIndex(1, 3)
     =#
     return findall(x -> x == a, array)
 end
